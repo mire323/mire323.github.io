f9dc8a014534b6dd0dd3fdf60f0b4b66590e53a5
mire323 <mirko.druzijanic@gmail.com>
2018-06-18 08:05:49 +0200

    SECOND UPDATE

--- js/main.js
+++ js/main.js
@@ -1,34 +1,52 @@
-;(function () {
-	
-	'use strict';
+$(document).ready(function() {
 
+  var form = $('#form'),
+      email = $('#email'),
+      
+      message = $('#message'),
+      submit = $("#submit");
+  
+  form.on('input', '#email, #message', function() {
+    $(this).css('border-color', '');
+    info.html('').slideUp();
+  });
+  
+  submit.on('click', function(e) {
+    e.preventDefault();
+    if(validate()) {
+      $.ajax({
+        type: "POST",
+        url: "kontakt.php",
+        data: form.serialize(),
+        dataType: "json"
+      }).done(function(data) {
+        if(data.success) {
+          email.val('');
+          
+          message.val('');
+          info.html('Message sent!').css('color', 'green').slideDown();
+        } else {
+          info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+        }
+      });
+    }
+  });
+  
+  function validate() {
+    var valid = true;
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    
+    if(!regex.test(email.val())) {
+      email.css('border-color', 'red');
+      valid = false;
+    }
+    
+    if($.trim(message.val()) === "") {
+      message.css('border-color', 'red');
+      valid = false;
+    }
+    
+    return valid;
+  }
 
-
-	// iPad and iPod detection	
-	var isiPad = function(){
-		return (navigator.platform.indexOf("iPad") != -1);
-	};
-
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
-			(navigator.platform.indexOf("iPod") != -1)
-	    );
-	};
-
-	var parallax = function() {
-		$(window).stellar({
-			horizontalScrolling: false,
-			hideDistantElements: false, 
-			responsive: true
-
-		});
-	};
-
-	// Document on load.
-	$(function(){
-		parallax();
-	});
-
-
-}());
+});
